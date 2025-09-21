9f12174831c644916df6b59e21a0736c3844c0d7
Vikash kumar <vraj67207@gmail.com>
2025-09-21 16:34:44 +0530

    minnor issue

--- auth-backend/models/User.js
+++ auth-backend/models/User.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: String,
-  phoneCode: { type: String, required: true },
-  phone: { type: String, required: true, unique: true },
+  email: { type: String, unique: true, sparse: true }, 
+  phoneCode: String,
+  phone: { type: String, required: true },
   password: String,
   otp: String,
   isVerified: { type: Boolean, default: false }
 });
+
 
 module.exports = mongoose.model("User", userSchema);
